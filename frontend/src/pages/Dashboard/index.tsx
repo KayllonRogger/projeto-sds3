596d0606d8f21840389bb675439f983ad623c19e
Kayllon Rogger <kayllonrogger@gmail.com>
2021-11-05 11:36:55 -0300

    Home, navigation

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -6,29 +6,28 @@ import NavBar from "components/NavBar";
 
 function Dashboard() {
     return (
-        <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
-            <>
-                <div className="container-fluid">
-                    <NavBar />
-                    <h1 className="text-primary py-3">Dashboard de Vendas</h1>
-                    <div className="row px-3">
-                        <div className="col-sm-6">
-                            <h5 className="text-center text-secondary">Taxa de Sucesso</h5>
-                            <BarCharts />
-                        </div>
-                        <div className="col-sm-6">
-                            <h5 className="text-center text-secondary">Todas as vendas</h5>
-                            <DonutsChart />
-                        </div>
+        <>
+            <NavBar />
+            <div className="container">
+
+                <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+                <div className="row px-3">
+                    <div className="col-sm-6">
+                        <h5 className="text-center text-secondary">Taxa de Sucesso</h5>
+                        <BarCharts />
                     </div>
-                    <div className="row py 3">
-                        <h1 className="text-primary">Todas as vendas</h1>
-                        <DataTable />
+                    <div className="col-sm-6">
+                        <h5 className="text-center text-secondary">Todas as vendas</h5>
+                        <DonutsChart />
                     </div>
-                    <Footer />
                 </div>
-            </>
-        </div>
+                <div className="row py 3">
+                    <h1 className="text-primary">Todas as vendas</h1>
+                    <DataTable />
+                </div>
+            </div>
+            <Footer />
+        </>
     );
 }
 
